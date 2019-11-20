d0967c3fb208a07119577e7a5d063b5bc9ba429d
Elliott Murray <elliottmurray@gmail.com>
2019-11-20 20:09:27 +1100

    oz deploy

--- elliott-kinesis/handler.js
+++ elliott-kinesis/handler.js
@@ -1,6 +1,26 @@
 'use strict';
+var AWS = require('aws-sdk');
 
 module.exports.pushEvent = async event => {
+  var kinesis = new AWS.Kinesis();
+  console.log("!!!!!!!!!");
+  console.log(process.env.OUR_STREAM)
+
+  console.log(event);
+  // streamName = event.get('streamName');
+  var streamName = '';
+  var params = {
+    Data: Buffer.from('hello world'), 
+    PartitionKey: '1',
+    StreamName: 'queensWorkStream'
+    
+  };
+  kinesis.putRecord(params, function(err, data) {
+    if (err) console.log(err, err.stack); // an error occurred
+    else     console.log(data);           // successful response
+  });
+
+  
   return {
     statusCode: 200,
     body: JSON.stringify(
@@ -12,7 +32,4 @@ module.exports.pushEvent = async event => {
       2
     ),
   };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
